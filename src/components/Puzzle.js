365456a9d8865875ed0f4fb16a630d46069b20ca
Jonathan Lahue <jonathanlahue@gmail.com>
2017-10-31 15:31:02 -0400

    added 4 new real puzzles to test. Puzzles up to Medium complexity validate properly

--- src/components/Puzzle.js
+++ src/components/Puzzle.js
@@ -20,6 +20,8 @@ export const entities = Entities;
 // null: the selector might be here
 // entity-property: indicates where another entity or entity property is relative to the selector.
 //
+
+// CUSTOM PUZZLE
 // const Puzzle = [
 //   [
 //     {
@@ -106,7 +108,8 @@ export const entities = Entities;
 //
 // ];
 
-const Puzzle = [
+// FACILE
+/*const Puzzle = [
   [
     {
   selectorImg: Entities[0].img,
@@ -190,265 +193,222 @@ const Puzzle = [
     ]
   }]
 
+];*/
+
+// MOYEN
+const Puzzle = [
+  [
+    {
+  selectorImg: Entities[0].img,
+  selectorName: Entities[0].name,
+  logicCells: [
+    [false,false,null],
+    [false,false,null],
+    [null,null,null]
+    ]
+  },
+  {
+  selectorImg: Entities[1].img,
+  selectorName: Entities[1].name,
+  logicCells: [
+    [false,false,false],
+    [false,null,false],
+    [null,null,null]
+    ]
+  },
+  {
+  selectorImg: Entities[2].img,
+  selectorName: Entities[2].name,
+  logicCells: [
+    [false,false,false],
+    [false,null,null],
+    [false,null,null]
+    ]
+  }],
+  [{
+  selectorImg: Entities[3].img,
+  selectorName: Entities[3].name,
+  logicCells: [
+    [null,null,null],
+    [null,null,null],
+    [null,null,true]
+    ]
+  },
+  {
+  selectorImg: Entities[4].img,
+  selectorName: Entities[4].name,
+  logicCells: [
+    [false,false,false],
+    [false,null,false],
+    [false,null,false]
+    ]
+  },
+  {
+  selectorImg: Entities[5].img,
+  selectorName: Entities[5].name,
+  logicCells: [
+    [null,null,null],
+    [null,null,null],
+    [null,true,null]
+    ]
+  }],
+  [{
+  selectorImg: Entities[6].img,
+  selectorName: Entities[6].name,
+  logicCells: [
+    [null,null,null],
+    [null,null,null],
+    [null,false,false]
+    ]
+  },
+  {
+  selectorImg: Entities[7].img,
+  selectorName: Entities[7].name,
+  logicCells: [
+    [null,null,null],
+    [false,false,false],
+    [null,null,null]
+    ]
+  },
+  {
+  selectorImg: Entities[8].img,
+  selectorName: Entities[8].name,
+  logicCells: [
+    [false,null,null],
+    [false,null,null],
+    [false,null,null]
+    ]
+  }]
+
 ];
-// 
-// const Puzzle = [
-//   [
-//     {
-//   selectorImg: Entities[0].img,
-//   selectorName: Entities[0].name,
-//   logicCells: [
-//     [true,false,false],
-//     [false,null,false],
-//     [false,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[1].img,
-//   selectorName: Entities[1].name,
-//   logicCells: [
-//     [false,true,false],
-//     [false,false,false],
-//     [false,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[2].img,
-//   selectorName: Entities[2].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,true,false],
-//     [false,false,false]
-//     ]
-//   }],
-//   [{
-//   selectorImg: Entities[3].img,
-//   selectorName: Entities[3].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,false,false],
-//     [false,false,true]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[4].img,
-//   selectorName: Entities[4].name,
-//   logicCells: [
-//     [false,false,true],
-//     [false,false,false],
-//     [false,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[5].img,
-//   selectorName: Entities[5].name,
-//   logicCells: [
-//     [false,false,false],
-//     [true,false,false],
-//     [false,false,false]
-//     ]
-//   }],
-//   [{
-//   selectorImg: Entities[6].img,
-//   selectorName: Entities[6].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,false,true],
-//     [false,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[7].img,
-//   selectorName: Entities[7].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,false,false],
-//     [true,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[8].img,
-//   selectorName: Entities[8].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,false,false],
-//     [false,true,null]
-//     ]
-//   }]
-//
-// ];
-//
-// const Puzzle = [
-//   [
-//     {
-//   selectorImg: Entities[0].img,
-//   selectorName: Entities[0].name,
-//   logicCells: [
-//     [true,false,false],
-//     [false,null,false],
-//     [false,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[1].img,
-//   selectorName: Entities[1].name,
-//   logicCells: [
-//     [false,true,false],
-//     [false,false,false],
-//     [false,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[2].img,
-//   selectorName: Entities[2].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,true,false],
-//     [false,false,false]
-//     ]
-//   }],
-//   [{
-//   selectorImg: Entities[3].img,
-//   selectorName: Entities[3].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,false,false],
-//     [false,false,true]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[4].img,
-//   selectorName: Entities[4].name,
-//   logicCells: [
-//     [false,false,true],
-//     [false,false,false],
-//     [false,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[5].img,
-//   selectorName: Entities[5].name,
-//   logicCells: [
-//     [false,false,false],
-//     [true,false,false],
-//     [false,false,false]
-//     ]
-//   }],
-//   [{
-//   selectorImg: Entities[6].img,
-//   selectorName: Entities[6].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,false,true],
-//     [false,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[7].img,
-//   selectorName: Entities[7].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,false,false],
-//     [true,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[8].img,
-//   selectorName: Entities[8].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,false,false],
-//     [false,true,null]
-//     ]
-//   }]
-//
-// ];
-//
-//
-// const Puzzle = [
-//   [
-//     {
-//   selectorImg: Entities[0].img,
-//   selectorName: Entities[0].name,
-//   logicCells: [
-//     [true,false,false],
-//     [false,null,false],
-//     [false,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[1].img,
-//   selectorName: Entities[1].name,
-//   logicCells: [
-//     [false,true,false],
-//     [false,false,false],
-//     [false,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[2].img,
-//   selectorName: Entities[2].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,true,false],
-//     [false,false,false]
-//     ]
-//   }],
-//   [{
-//   selectorImg: Entities[3].img,
-//   selectorName: Entities[3].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,false,false],
-//     [false,false,true]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[4].img,
-//   selectorName: Entities[4].name,
-//   logicCells: [
-//     [false,false,true],
-//     [false,false,false],
-//     [false,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[5].img,
-//   selectorName: Entities[5].name,
-//   logicCells: [
-//     [false,false,false],
-//     [true,false,false],
-//     [false,false,false]
-//     ]
-//   }],
-//   [{
-//   selectorImg: Entities[6].img,
-//   selectorName: Entities[6].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,false,true],
-//     [false,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[7].img,
-//   selectorName: Entities[7].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,false,false],
-//     [true,false,false]
-//     ]
-//   },
-//   {
-//   selectorImg: Entities[8].img,
-//   selectorName: Entities[8].name,
-//   logicCells: [
-//     [false,false,false],
-//     [false,false,false],
-//     [false,true,null]
-//     ]
-//   }]
-//
-// ];
+
+// Difficile
+/*const Puzzle = [
+  [
+    {
+  selectorImg: Entities[0].img,
+  selectorName: Entities[0].name,
+  logicCells: [
+    [false,null,null]
+    ]
+  },
+  {
+  selectorImg: Entities[1].img,
+  selectorName: Entities[1].name,
+  logicCells: [
+    [false,null,false]
+    ]
+  },
+  {
+  selectorImg: Entities[2].img,
+  selectorName: Entities[2].name,
+  logicCells: [
+    ["red-circle"],
+    [true]
+    ]
+  }],
+  [{
+  selectorImg: Entities[3].img,
+  selectorName: Entities[3].name,
+  logicCells: [
+    ["empty",true],
+    ["yellow-triangle","empty"]
+    ]
+  },
+  {
+  selectorImg: Entities[4].img,
+  selectorName: Entities[4].name,
+  logicCells: [
+    [false,"empty","empty"],
+    ["empty",false,"empty"],
+    ["empty","empty",false]
+    ]
+  },
+  {
+  selectorImg: Entities[5].img,
+  selectorName: Entities[5].name,
+  logicCells: [
+    ["empty","empty",null],
+    [true,null,"empty"]
+    ]
+  }],
+  [{
+  selectorImg: Entities[6].img,
+  selectorName: Entities[6].name,
+  logicCells: [
+    ["yellow-circle",null,true]
+    ]
+  },
+  {
+  selectorImg: Entities[7].img,
+  selectorName: Entities[7].name,
+  logicCells: [
+    [null],
+    [false],
+    [null]
+    ]
+  },
+  {
+  selectorImg: Entities[8].img,
+  selectorName: Entities[8].name,
+  logicCells: [
+    [false,false,false],
+    [false,false,false],
+    ["empty",false,false]
+    ]
+  }]
+
+];*/
+
+
+// Très difficile
+/*const Puzzle = [
+  [
+    {
+  selectorImg: Entities[0].img,
+  selectorName: Entities[0].name,
+  logicCells: [
+    [true,true,true],
+    ["empty","triangle","empty"]
+    ]
+  },
+  {
+  selectorImg: Entities[1].img,
+  selectorName: Entities[1].name,
+  logicCells: [
+    ["empty",true],
+    ["yellow",true],
+    ["empty",true]
+    ]
+  },
+  {
+  selectorImg: Entities[2].img,
+  selectorName: Entities[2].name,
+  logicCells: [
+    [false,false,false],
+    [null,null,null],
+    [null,null,null]
+    ]
+  }],
+  [{
+  selectorImg: Entities[3].img,
+  selectorName: Entities[3].name,
+  logicCells: [
+    [false,false,false],
+    [false,null,false],
+    [null,null,null]
+    ]
+  },
+  {
+  selectorImg: Entities[4].img,
+  selectorName: Entities[4].name,
+  logicCells: [
+    [null,false,null],
+    [false,null,null],
+    [null,false,null]
+    ]
+  }]
+
+];*/
 
 export default Puzzle;
