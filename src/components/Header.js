8337024a74dff7444408f9394bde60d55f43185f
Jonathan Lahue <jonathanlahue@gmail.com>
2017-11-26 16:54:42 -0500

    made navbar responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,17 @@ import logo from '../assets/logike_white_trimmed.png';
 
 const Header = () => {
 
+// changes the class name to trigger CSS to open the nav bar
+const openNav = (event) => {
+  let nav = event.target.parentElement.parentElement.parentElement;
+  if(nav.className === "open-nav") {
+    nav.className = "closed-nav";
+
+  }
+  else {
+    nav.className = "open-nav";
+  }
+}
 
 // TODO: make the nav responsive
   return (
@@ -12,17 +23,16 @@ const Header = () => {
         <nav>
           <div className="float-wrapper">
             <div id="nav-logo">
-
               <a rel="home" href="/" title="Home">
                 <img src={logo}/>
               </a>
             </div>
-            <a id="nav-burger" href="#"><i className="fa fa-navicon fa-2x" ></i></a>
+            <a id="nav-burger" onClick={openNav}><i className="fa fa-navicon fa-2x" ></i></a>
 
             <ul>
+              <li><a href="https://www.scolab.com/">Scolab</a></li>
               <li><a href="http://www.confusedretriever.com/">Blog</a></li>
               <li><a href="https://twitter.com/jclahoot"><i className="fa fa-twitter" ></i></a></li>
-              <li><a href="https://www.scolab.com/">Scolab</a></li>
             </ul>
           </div>
         </nav>
