90051404f537a3f02d28344a51615064dab7b0d5
Jonathan Lahue <jonathanlahue@gmail.com>
2017-11-26 22:47:55 -0500

    fixed header anim bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,20 @@ import logo from '../assets/logike_white_trimmed.png';
 const Header = () => {
 
 // changes the class name to trigger CSS to open the nav bar
-const openNav = (event) => {
+const toggleNav = (event) => {
   let nav = event.target.parentElement.parentElement.parentElement;
   if(nav.className === "open-nav") {
     nav.className = "closed-nav";
-
+  // resets the class name so that the CSS animation won't run every single time screen is downsized
+    setTimeout(function () {
+      nav.className = "";
+    }, 500); //Must be longer or same as CSS anim length for selector: .closed-nav
   }
   else {
     nav.className = "open-nav";
   }
 }
 
-// TODO: make the nav responsive
   return (
     <div>
       <header>
@@ -27,7 +29,7 @@ const openNav = (event) => {
                 <img src={logo}/>
               </a>
             </div>
-            <a id="nav-burger" onClick={openNav}><i className="fa fa-navicon fa-2x" ></i></a>
+            <a id="nav-burger" onClick={toggleNav}><i className="fa fa-navicon fa-2x" ></i></a>
 
             <ul>
               <li><a href="https://www.scolab.com/">Scolab</a></li>
