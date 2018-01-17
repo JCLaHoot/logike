b2bd2ee295c041ba25e3c8be511a35553bf7914b
Jonathan Lahue <jonathanlahue@gmail.com>
2018-01-16 23:23:26 -0500

    started adding sliders to choose the puzzle size

--- src/components/PuzzleBuilder/ChoosePuzzleSize.jsx
+++ src/components/PuzzleBuilder/ChoosePuzzleSize.jsx
@@ -7,6 +7,15 @@ const ChoosePuzzleSize = ({chooseSize}) => {
     <div className="choose-puzzle-size">
       <h4>Choose the puzzle size</h4>
 
+            <input type="range" min="1" max="5" class="slider horizontal-slider" id="myRange"/>
+            <div className="float-wrapper">
+              <div className="vertical-slider-wrapper">
+                <input type="range" min="1" max="5" class="slider vertical-slider" id="myRange"/>
+              </div>
+              <div class="puzzle-size-preview">
+                grid
+              </div>
+            </div>
     </div>
   )
 
