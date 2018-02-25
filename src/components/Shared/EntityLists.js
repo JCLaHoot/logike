2d5fd615a211b0d6c0f14526ce6c8c11274d72d3
Jonathan Lahue <jonathanlahue@gmail.com>
2018-02-25 15:49:19 -0500

    added animals entity list

--- src/components/Shared/EntityLists.js
+++ src/components/Shared/EntityLists.js
@@ -1,10 +1,11 @@
 import eColoredShapes from './eColoredShapes';
 import eFruitsAndVegetables from './eFruitsAndVegetables';
+import eAnimals from './eAnimals';
 
 
 // TODO: add more than one entity set;
 // TODO: if editable, will need to implement editability.
 
-const EntityLists = [eColoredShapes, eFruitsAndVegetables];
+const EntityLists = [eColoredShapes, eFruitsAndVegetables, eAnimals];
 
 export default EntityLists;
